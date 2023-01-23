67760d2546ce972ce86191c4210c12b4461dfc54
Frédéric Gaufichon <fgaufichon@hotmail.fr>
2023-01-23 15:45:00 +0100

    need to resolve setTokem

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,43 +1,46 @@
-import React, { useState } from "react";
-/* import "./Login.css";
- */
-const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  function validateForm() {
-    return email.length > 0 && password.length > 0;
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+async function loginUser(credentials) {
+  return fetch('http://localhost:3001/login', { 
+    method: 'POST', 
+    headers: {
+       'Content-Type': 'application/json' 
+    }, 
+    body: JSON.stringify(credentials) 
+  })
+     .then(data => data.json())
   }
-
-  function handleSubmit(event) {
-    event.preventDefault();
+  export default function Login({ setToken }) { 
+   const [username, setUserName] = useState(); 
+   const [password, setPassword] = useState();
+    const handleSubmit = async e => { 
+     e.preventDefault(); 
+     const token = await loginUser({ 
+       username, 
+       password 
+     }); 
+     setToken(token); 
+   } 
+   return( 
+     <div className="login-wrapper"> 
+       <h1>Please Log In</h1> 
+       <form onSubmit={handleSubmit}> 
+         <label>
+            <p>Username</p> 
+           <input type="text" onChange={e => setUserName(e.target.value)} />
+          </label>
+          <label> 
+           <p>Password</p> 
+           <input type="password" onChange={e => setPassword(e.target.value)} />
+          </label>
+          <div>
+            <button type="submit">Submit</button> 
+         </div>
+        </form>
+      </div>
+    )
   }
-  return (
-    <div className="Login">
-      <Form onSubmit={handleSubmit}>
-        <Form.Group size="lg" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            autoFocus
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-
-        </Form.Group>
-        <Form.Group size="lg" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button block size="lg" type="submit" disabled={!validateForm()}>
-          Login
-        </Button>
-      </Form>
-    </div>
-  );
-}
-
-export default Login;
+  Login.propTypes = {
+    setToken: PropTypes.func.isRequired 
+ };
